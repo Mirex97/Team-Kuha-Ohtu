221f1f980f4394b6c7a2b2f38a47651a66ce309b
EssKayz <esskay@protonmail.com>
2018-12-08 13:36:40 +0200

    Update InputValidator.java
    MODIFIED >
     InputValidator.java
        - Refactored the if statements to instead implement a switch case by isbn length.

--- src/main/java/bookmarks/ui/InputValidator.java
+++ src/main/java/bookmarks/ui/InputValidator.java
@@ -1,76 +1,81 @@
 package bookmarks.ui;
 
 public class InputValidator {
-	public static boolean validateISBN(String isbn) {
-		if (isbn == null) {
-			return false;
-		}
 
-		isbn = isbn.replaceAll("[- ]", "");
+    public static boolean validateISBN(String isbn) {
+        if (isbn == null) {
+            return false;
+        }
 
-		
-		if (isbn.length() == 10) {
-			try {
-				int checksum = calculateISBN10Checksum(isbn);
-				
-				if (checksum == 10) {
-					return ("" + isbn.charAt(9)).toLowerCase().equals("x");
-				}
+        isbn = isbn.replaceAll("[- ]", "");
 
-				return checksum == Integer.parseInt("" + isbn.charAt(9));
-			} catch (Exception e) {
-				return false;
-			}
-		}
-			
-		if (isbn.length() != 13) {
-			return false;
-		}
+        switch (isbn.length()) {
+            case 10: {
+                try {
+                    int checksum = calculateISBN10Checksum(isbn);
 
-		if (!isbn.substring(0, 3).equals("978") && !isbn.substring(0, 3).equals("979")) {
-			return false;
-		}
+                    if (checksum == 10) {
+                        return ("" + isbn.charAt(9)).toLowerCase().equals("x");
+                    }
 
-		try {
-			int checksum = calculateISBN13Checksum(isbn);
+                    return checksum == Integer.parseInt("" + isbn.charAt(9));
+                } catch (Exception e) {
+                    return false;
+                }
+            }
 
-			return checksum == Integer.parseInt("" + isbn.charAt(12));
-		} catch (Exception e) {
-			return false;
-		}
-	}
-	
-	private static int calculateISBN10Checksum(String isbn) throws NumberFormatException {
-		int total = 0;
-		for (int i = 0; i < 9; i++) {
-			int digit = Integer.parseInt("" + isbn.charAt(i));
-			total += (10 - i) * digit;
-		}
+            case 13: {
+                if (!isbn.substring(0, 3).equals("978") && !isbn.substring(0, 3).equals("979")) {
+                    return false;
+                }
 
-		return (11 - (total % 11)) % 11;
-	}
-	
-	private static int calculateISBN13Checksum(String isbn) throws NumberFormatException {
-		int total = 0;
-		for (int i = 0; i < 12; i++) {
-			int digit = Integer.parseInt("" + isbn.charAt(i));
-			if (i % 2 == 0) {
-				total += digit;
-			} else {
-				total += digit * 3;
-			}
-		}
+                try {
+                    int checksum = calculateISBN13Checksum(isbn);
 
-		int checksum = 10 - (total % 10);
-		if (checksum == 10) {
-			checksum = 0;
-		}
-		
-		return checksum;
-	}
+                    return checksum == Integer.parseInt("" + isbn.charAt(12));
+                } catch (Exception e) {
+                    return false;
+                }
+            }
 
-	public static boolean validateLink(String input) {
-		String urlPattern = "^(http(s?)://)?[a-zA-Z0-9_\\-]+\\.([A-Za-z/]{2,5})[a-zA-Z0-9_/\\&\\?\\=\\-\\.\\~\\%]*";
-		return input.matches(urlPattern);
-	}
+            default: {
+                return false;
+            }
+        }
+
+    }
+
+    private static int calculateISBN10Checksum(String isbn) throws NumberFormatException {
+        int total = 0;
+        for (int i = 0; i < 9; i++) {
+            int digit = Integer.parseInt("" + isbn.charAt(i));
+            total += (10 - i) * digit;
+        }
+
+        return (11 - (total % 11)) % 11;
+    }
+
+    private static int calculateISBN13Checksum(String isbn) throws NumberFormatException {
+        int total = 0;
+        for (int i = 0; i < 12; i++) {
+            int digit = Integer.parseInt("" + isbn.charAt(i));
+            if (i % 2 == 0) {
+                total += digit;
+            } else {
+                total += digit * 3;
+            }
+        }
+
+        int checksum = 10 - (total % 10);
+        if (checksum == 10) {
+            checksum = 0;
+        }
+
+        return checksum;
+    }
+
+    public static boolean validateLink(String input) {
+        String urlPattern = "^(http(s?)://)?[a-zA-Z0-9_\\-]+\\.([A-Za-z/]{2,5})[a-zA-Z0-9_/\\&\\?\\=\\-\\.\\~\\%]*";
+        return input.matches(urlPattern);
+    }
 }
